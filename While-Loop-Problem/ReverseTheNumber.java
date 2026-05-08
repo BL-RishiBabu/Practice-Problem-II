1bdf4e03bcb3b7e1acf799e83602f1932f79af24
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-08 16:20:34 +0530

    Program to reverse the integer number is executed

--- While-Loop-Problem/ReverseTheNumber.java
+++ While-Loop-Problem/ReverseTheNumber.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ReverseTheNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
         int num = sc.nextInt();
         int ans = 0;
 
