046a2b4912660135734a73d2e2e3f7b540c3aaa5
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-08 15:52:15 +0530

    Program for the sum of n natural numbers is executed.

---
+++ While-Loop-Problem/SumOfNaturalNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//  Program for the sum of n natural numbers
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int num = sc.nextInt();
+        int ans = 0;
+        int i = 1;
+
+        while (i <= num) {
+            ans += i;
+            i++;
+        }
+
+        System.out.println("Sum of " + num + " Natural Number is " + ans);
+    }
+}
